0ac1c8e9b20e1858b0177bcafd8f8c13029bc2b1
Quang-Tr <82781956+Quang-Tr@users.noreply.github.com>
2023-12-02 15:26:45 +0100

    2023 day 2

--- 2023/day1.py
+++ 2023/day1.py
@@ -4,34 +4,18 @@ import re
 day = Path(__file__).stem[3:]
 inputFile = Path(__file__).with_name(f"input{day}.txt")
 
-# Part 1
-sum1 = 0
-with open(inputFile) as input:
-    for line in input:
-        digits = re.findall(r"\d", line)
-        sum1 += int(digits[0] + digits[-1])
-print("Part 1:", sum1)
+def extract(line: str) -> int:
+    digits = re.findall(r"\d", line)
+    return int(digits[0] + digits[-1])
 
-# Part 2
-sum2 = 0
+sum1 = sum2 = 0
 keys = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 spelled = dict(zip(keys, "123456789"))
 with open(inputFile) as input:
     for line in input:
-        firstKeyOccurrence = (len(line), "")
+        sum1 += extract(line)
         for key in keys:
-            if key in line and (minIndex := line.index(key)) < firstKeyOccurrence[0]:
-                firstKeyOccurrence = (minIndex, key)
-        if toBeReplaced := firstKeyOccurrence[1]:
-            line = line.replace(toBeReplaced, spelled[toBeReplaced] + toBeReplaced[-1], 1)
-        
-        lastKeyOccurrence = (-1, "")
-        for key in keys:
-            if key in line and (maxIndex := line.rindex(key)) > lastKeyOccurrence[0]:
-                lastKeyOccurrence = (maxIndex, key)
-        if toBeReplaced := lastKeyOccurrence[1]:
-            line = line.replace(toBeReplaced, spelled[toBeReplaced])
-        
-        digits = re.findall(r"\d", line)
-        sum2 += int(digits[0] + digits[-1])
+            line = re.sub(key, key[0] + spelled[key] + key[-1], line)
+        sum2 += extract(line)
+print("Part 1:", sum1)
 print("Part 2:", sum2)
---
+++ 2023/day2.py
@@ -0,0 +1,27 @@
+# Day 2: Cube Conundrum
+from pathlib import Path
+import re
+day = Path(__file__).stem[3:]
+inputFile = Path(__file__).with_name(f"input{day}.txt")
+
+sum1 = sum2 = 0
+config = {"red": 12, "green": 13, "blue": 14}
+with open(inputFile) as input:
+    for game in input:
+        cubes = re.split(r"\W+", game)
+        gameID = int(cubes[1])
+        possible = True
+        power = 1
+        for color in config:
+            if color in cubes:
+                maxQuant = max(int(cubes[i - 1]) for i, c in enumerate(cubes) if c == color)
+                if maxQuant > config[color]:
+                    possible = False
+                power *= maxQuant
+            else:
+                power = 0
+        if possible:
+            sum1 += gameID
+        sum2 += power
+print("Part 1:", sum1)
+print("Part 2:", sum2)
